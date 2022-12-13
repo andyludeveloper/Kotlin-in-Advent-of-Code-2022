68c9aff95201b0d67e47f6574a9560fe76366507
Andy Lu <alu@futuredial.com>
2022-12-13 11:26:47 +0800

    [Day05] part2

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,19 +1,22 @@
 fun main() {
 
     val testInput = readInput("Day05_test")
-    println(part1(testInput))
-    check(part1(testInput) == "CMZ")
+    println(part1(testInput, 1))
+    println(part1(testInput, 2))
+    check(part1(testInput, 1) == "CMZ")
+    check(part1(testInput, 2) == "MCD")
 
     val input = readInput("Day05")
-//    println(input)
-    val part1 = part1(input)
+    val part1 = part1(input, 1)
+    val part2 = part1(input, 2)
     println(part1)
+    println(part2)
 }
 
-private fun part1(input: List<String>): String {
+private fun part1(input: List<String>, method: Int): String {
     val index = input.indexOf("")
     val cratesCount = input[index - 1].count { it.isDigit() }
-    val crates = List(cratesCount) { mutableListOf<Char>() }
+    val crates = MutableList(cratesCount) { mutableListOf<Char>() }
     val inputCrates = input.subList(0, index + 1)
     val action = input.subList(index + 1, input.size)
 
@@ -32,13 +35,14 @@ private fun part1(input: List<String>): String {
         val count = countS.trim().toInt()
         val from = fromS.trim().toInt() - 1
         val to = toS.trim().toInt() - 1
-
-        for (i in 0 until count) {
-            if (crates[from].isNotEmpty()) {
-
-                crates[to] += crates[from].last()
-                crates[from].removeLast()
+        if (method == 1) {
+            for (i in 0 until count) {
+                crates[to] += crates[from].takeLast(1)
+                crates[from] = crates[from].dropLast(1).toMutableList()
             }
+        } else {
+            crates[to] += crates[from].takeLast(count)
+            crates[from] = crates[from].dropLast(count).toMutableList()
         }
         moveIndex++
     }
