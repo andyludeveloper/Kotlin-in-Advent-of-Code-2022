684cf9ff315f15bf29914ca25b44cca87ceeeedf
Andy Lu <alu@futuredial.com>
2022-12-13 14:51:27 +0800

    [Day06] part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -3,30 +3,23 @@ fun main() {
     val test2 = "nppdvjthqldpwncqszvftbrmjlhg"
     val test3 = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
     val test4 = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
-    println(part1(test1))
-    check(part1(test1) == 5)
-    check(part1(test2) == 6)
+    println(solution(test1, 1))
+    check(solution(test1, 1) == 5)
+    check(solution(test2, 1) == 6)
 
-    check(part1(test3) == 10)
-    check(part1(test4) == 11)
+    check(solution(test3, 1) == 10)
+    check(solution(test4, 1) == 11)
 
     val input = readInputToText("Day06")
-    println(part1(input))
+    println(solution(input, 1))
+
+    val test5 = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+    check(solution(test5, 2) == 19)
+    println(solution(input, 2))
 }
 
-fun part1(string: String): Int {
-    return string.toCharArray().toList()
-        .windowed(4)
-        .map { it.toSet() }
-        .map { it.size == 4 }
-        .indexOf(true) + 4
-//    var result = 4
-//    println(windowed.indexOf(true))
-//    for (index in windowed.indices) {
-//        if (windowed[index]) {
-//            result += index
-//            break
-//        }
-//    }
-//    return result
+fun solution(string: String, part: Int): Int {
+    val windowSize = if (part == 1) 4 else 14
+    return string.toCharArray().toList().windowed(windowSize).map { it.toSet() }.map { it.size == windowSize }
+        .indexOf(true) + windowSize
 }
